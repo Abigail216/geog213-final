43b3c9b8579ea1901786565d5b3ac449524cd9f6
Abigail216 <phillipsabigail16@gmail.com>
2024-12-18 22:48:15 -0500

    Uploaded final report with updated readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# geog213-final
+# Geog213-Final
+This repository contains the Jupyter Notebook Report for my GEOG213 Final Project, which is on calculating the dNBR of the Butte County Dixie Fire 
+
+
+## Requirements
+
+You need to have Docker installed on your machine. 
+
+
+## Instructions
+
+It's recommended to pull the Docker image from Dockerhub. Otherwise, if you prefer, you can build your own image using the instructions in the following section. 
+
+```
+docker pull abphill2004/geog213-final:latest
+```
+
+```
+docker run -it -p 8888:8888 abphill2004/geog213-final:latest
+```
+
+- Copy the Jupyter Lab url and paste it in your browser. 
+- Open `geog213_phillips_report.ipynb`and `utils.py`to see the code
+
+
+Build the Docker image:
+
+**Windows users:** It is highly recommended that you pull the Docker image, there seems to be an issue with conda-forge on WSL. 
+
+```
+docker build -t geog213-final .
+```
+
+Run the container as following after switching to the repository's directory locally:
+```
+docker run -it -p 8888:8888 geog213-final
+```
+
+- Copy the Jupyter Lab url and paste it in your browser. 
+- Open `geog213_phillips_report.ipynb`and `utils.py`to see the code
--- utils.py
+++ utils.py
@@ -1,54 +1,28 @@
 from pystac_client import Client
 import matplotlib.pyplot as plt
+import rioxarray as rxr
+import matplotlib as plt
+import numpy as np
 
-__all__ = ['search_api']
+__all__ = ['search_api', 'normalize']
 
-def search_api(datetime, point)
+def search_api(datetime, point):
     catalog = Client.open("https://earth-search.aws.element84.com/v1")
         
     search = catalog.search(
         collections=["sentinel-2-l2a"],
         intersects=point,
         datetime=datetime,
-        query=["eo:cloud_cover<5", "s2: snow_and_ice_percentage<5"
         )
 
-    return search.matched()
-
-
-def gen_stac_asset_urls(items, asset):
-    """
-    This function receives an items collection returned by a STAC API, and returns
-    the urls of the requested `asset` in a list. 
-
-    Inputs:
-        items : json collection
-            A STAC items collection returned by STAC API
-        asset : string
-            Name of an asset present in the `items` collection
-
-    Returns:
-        urls : list
-            List of all usls related to the `asset`
-            
-    """
+    items = search.item_collection()
+    print(len(items))
     
-    urls = []
-    for item in items:
-        urls.append(item.assets[asset].href)
-    
-    return urls
+    return items
 
-def gen_map(band1, band2, selected_pre_image, selected_post_image):
-    assets_pre = selected_pre_image.assets
-    assets_post = selected_post_image.assets
-    
-    nir_pre_href = assets_pre[band1].href
-    print(nir_pre_href)
+def normalize(array): 
+    array_max = array.max() 
+    array_min = array.min()
+    return ((array-array_min)/(array_max-array_min))
 
-    swir_pre_href = assets_pre[band2].href
-    print(swir_pre_href)
-
-    nir = rxr.open_rasterio(nir_pre_href)
-    plt.imshow(nir[0,:,:], cmap="magma_r")
     
